2e1098440e7393f6d905e0cf9f34c5871d57d3dd
Dane <dane160290@yahoo.de>
2019-02-09 16:03:39 +0100

    add spring init files

---
+++ src/main/java/de/spring/treg/tournamentregistration/TournamentRegistrationApplication.java
@@ -0,0 +1,14 @@
+package de.spring.treg.tournamentregistration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TournamentRegistrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TournamentRegistrationApplication.class, args);
+	}
+
+}
+
